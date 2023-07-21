1d61e32d5b7534c9423fbec67c7f92cc1ab2e109
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2023-07-21 17:22:07 +0200

    v1.4.1

--- Cheater_Priority.lua
+++ Cheater_Priority.lua
@@ -24,7 +24,7 @@ local players = entities.FindByClass("CTFPlayer")
 local options = {
     StrikeLimit = 10,
     MaxTickDelta = 8,
-    AimbotSensetivity = 0.4,
+    AimbotSensetivity = 0.1,
     AutoMark = true,
 }
 
@@ -76,7 +76,9 @@ end
 -- Detects rage pitch (looking up/down too much)
 local function CheckPitch(player, entity)
     local angles = player:GetEyeAngles()
-    if angles.pitch >= 89 or angles.pitch <= -89 then
+    print(angles.pitch)
+    if angles.pitch == 89.00 or angles.pitch == -89.00 -- aa pitch when normaly only 89.29 max = hard to reproduce when legit
+    or angles.pitch >= 89.30 or angles.pitch <= -89.30 then -- aa pitch but broken with exploits
         StrikePlayer(player:GetIndex(), "Invalid pitch", entity)
     end
 end
